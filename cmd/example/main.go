a155cdb2c1a22a580b343f7e97268e667fdcc770
y-miyazaki <ymiyazakixyz@gmail.com>
2020-07-14 23:28:32 +0000

    test

--- cmd/example/main.go
+++ cmd/example/main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 func main() {
 	fmt.Println("hello world.")
+	example()
+}
+
+func example() error {
+	return errors.New("error test")
 }
